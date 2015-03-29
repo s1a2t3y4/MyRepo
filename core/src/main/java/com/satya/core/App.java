49549c94dc5cf17bb1080b65d71ca8c1420f56d0
s1a2t3y4 <kovvursatti@gmail.com>
2015-03-29 16:46:01 +0530

    initial commit n# with '#' will be ignored, and an empty message aborts the commit.

---
+++ core/src/main/java/com/satya/core/App.java
@@ -0,0 +1,13 @@
+package com.satya.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
